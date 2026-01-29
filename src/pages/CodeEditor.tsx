cde6e2809e0348f69538b9022e41100535d954ee
sudev <sudev1997@gmail.com>
2026-01-29 09:37:47 +0530

    codeeditor

--- src/pages/CodeEditor.tsx
+++ src/pages/CodeEditor.tsx
@@ -130,7 +130,7 @@ const CodeEditor = () => {
     <main className="pt-16 min-h-screen bg-background">
       {/* Toolbar */}
       <div className="border-b border-border bg-card">
-        <div className="container mx-auto">
+        <div className=" mx-auto pl-4 pr-8 border">
           <div className="flex items-center justify-between h-14">
             {/* Left side */}
             <div className="flex items-center gap-4">
